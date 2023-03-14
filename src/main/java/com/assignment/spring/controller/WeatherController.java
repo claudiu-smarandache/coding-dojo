5ddbfcfdb2699e19e7ac862a457a6ef60ba0ccec
Claudiu Smarandache <claudiu.smarandache@yahoo.com>
2023-03-15 00:19:29 +0200

    Removed constants, moved to application.yml

--- src/main/java/com/assignment/spring/controller/WeatherController.java
+++ src/main/java/com/assignment/spring/controller/WeatherController.java
@@ -1,7 +1,10 @@
-package com.assignment.spring;
+package com.assignment.spring.controller;
 
+import com.assignment.spring.model.WeatherEntity;
+import com.assignment.spring.WeatherRepository;
 import com.assignment.spring.api.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +21,16 @@ public class WeatherController {
     @Autowired
     private WeatherRepository weatherRepository;
 
+    @Value("${api.key}")
+    private String apiKey;
+
+    @Value("${api.url}")
+    private String apiUrl;
+
     @RequestMapping("/weather")
     public WeatherEntity weather(HttpServletRequest request) {
         String city = request.getParameter("city");
-        String url = Constants.WEATHER_API_URL.replace("{city}", city).replace("{appid}", Constants.APP_ID);
+        String url = apiUrl.replace("{city}", city).replace("{appid}", apiKey);
         ResponseEntity<WeatherResponse> response = restTemplate.getForEntity(url, WeatherResponse.class);
         return mapper(response.getBody());
     }
